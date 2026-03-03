367385c8ff80dd8affbd6adda4eb6c0bc76fcd9c
zaidanahd <ahmadzaidan0512@gmail.com>
2026-03-03 15:57:51 +0700

    latihan modul2

---
+++ src/LatihanModul2.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class LatihanModul2 {
+    public static void main(String[] args) throws Exception {
+        String nama = " ZAIDAN " ;
+        
+        
+        int a = 2; int b = 10;
+        int c = 3; int d = 20;
+        double hasil = d/c;
+
+        System.out.println("hasil perkalian 2 dan 3= " + (a*b));
+        System.out.println("hasil pembagian 20 dan 3 = " + (d/c));
+        System.out.println("hasil pembagian 20 dan 3 = " + ((double)d/c));
+        System.out.println("hasil pembagian 20 dan 3 = " + hasil );
+        System.out.println("modulus = " + (d%c));
+        System.out.println("Hello, World!");
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("masukkan tanggal Lahir = ");
+        int tanggalLahir = input.nextInt();
+
+        input.nextLine();
+
+        System.out.println("masukkan nama Lengkap = ");
+        String namaLengkap = input.nextLine();
+
+        System.out.println(nama + " " + tanggalLahir);
+        input.close();
+
+        
+
+
+    }
+}
